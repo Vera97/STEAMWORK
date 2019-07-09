9232d9eb822437072ce0e2a94627b797a623ea8d
MHX789 <1356794938@qq.com>
2019-07-09 22:56:33 +0800

    Update Introduction.vue

--- src/components/Course/Introduction.vue
+++ src/components/Course/Introduction.vue
@@ -3,9 +3,11 @@
             <el-card :body-style="{ padding: '14px' }" class="card-position">
                 <span>&nbsp;课程介绍：<br> &emsp;&emsp;简介如下，简介如下，简介如下，简介如下,简介如下，简介如下，简介如下，简介如下，简介如下,简介如下，简介如下，简介如下
                 简介如下，简介如下，简介如下.</span>
-                <el-card class="box-card">
-                    <a href="#"><i class="el-icon-video-camera-solid "></i></a>
-                </el-card>
+                    <video width="100%" height="100%" controls>
+                        <source src="movie.mp4" type="video/mp4">
+                        <source src="movie.ogg" type="video/ogg">
+                        您的浏览器不支持Video标签。
+                    </video>
             </el-card>
     </el-row>
 </template>
@@ -21,7 +23,7 @@
         width:100%;
     }
     .box-card{
-        font-size:110px;
-        text-align:center;
+        width:100%;
+        height:100%;
     }
 </style>
