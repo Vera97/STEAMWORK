716b3997227b7c632abc362baa4f7455f5aefba9
MHX789 <1356794938@qq.com>
2019-07-09 17:48:39 +0800

    2019.7.9
    #新增了课程信息介绍的静态页面

--- src/components/Course/Related.vue
+++ src/components/Course/Related.vue
@@ -1,13 +1,30 @@
 <template>
-
+    <el-row class="el-row">
+        <h3>相关课程</h3>
+        <el-col :span="4" v-for="(o, index) in 5" :key="o" :offset="index > 0 ? 1 : 0">
+            <CourseCell :title="title+o" :introduction="introduction+o" :id="123"></CourseCell>
+        </el-col>
+    </el-row>
 </template>
 
 <script>
+    import CourseCell from "../Course-cell";
     export default {
-        name: "Related"
+        name: "Related",
+        components: {CourseCell},
+        data(){
+            return {
+                title:'其他课程',
+                introduction:'一句话的简介xxxx',
+            }
+        }
     }
 </script>
 
 <style scoped>
+    .el-row{
+        width: 100%;
+    }
 
 </style>
+>
