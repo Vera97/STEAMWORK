87050217303e9ce426107c55fc8608b4aaa2a257
MHX789 <52394333+MHX789@users.noreply.github.com>
2019-07-14 23:28:29 +0800

    Add files via upload

--- src/components/Home/Search.vue
+++ src/components/Home/Search.vue
@@ -1,115 +1,207 @@
 <template>
-  <div class="search-wrapper">
-    <el-form :inline="true">
-      <el-form-item class="search">
-        <el-input placeholder="输入关键字" v-model="keyword"></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="search">搜索</el-button>
-      </el-form-item>
-      <el-form-item class="filter">
-      <el-select v-model="selected.kind" placeholder="<请选择类别>">
-        <el-option :label="'<请选择类别>'" :value="''"></el-option>
-        <el-option v-for="item in conditions.kind" :key="item.type" :label="item.type" :value="item.type"></el-option>
-      </el-select>
-      </el-form-item>
-      <el-form-item class="filter">
-      <el-select v-model="selected.class" placeholder="<请选择课程>">
-        <el-option :label="'<请选择课程>'" :value="''"></el-option>
-        <el-option v-for="item in conditions.class" :key="item.type" :label="item.type" :value="item.type"></el-option>
-      </el-select>
-      </el-form-item>
-      <el-form-item class="filter">
-      <el-select v-model="selected.teacher" placeholder="<请选择老师>">
-        <el-option :label="'<请选择老师>'" :value="''"></el-option>
-        <el-option v-for="item in conditions.teacher" :key="item.type" :label="item.type" :value="item.type"></el-option>
-      </el-select>
-      </el-form-item>
-      <el-form-item class="filter">
-      <el-select v-model="selected.time" placeholder="<请选择时长>">
-        <el-option :label="'<请选择时长>'" :value="''"></el-option>
-        <el-option v-for="item in conditions.time" :key="item.type" :label="item.type" :value="item.type"></el-option>
-      </el-select>
-      </el-form-item>
-      <el-form-item class="filter">
-      <el-select v-model="selected.other" placeholder="<请选择其它条件>">
-        <el-option :label="'<请选择其它条件>'" :value="''"></el-option>
-        <el-option v-for="item in conditions.other" :key="item.type" :label="item.type" :value="item.type"></el-option>
-      </el-select>
-      </el-form-item>
-      <el-form-item>
-      <el-button type="primary" class="filter-button" @click="filter">筛选</el-button>
-      </el-form-item>
-    </el-form>
+  <div class="container-fluid">
+    <div class="container-fluid search-box-wrapper">
+      <ol class="breadcrumb pull-right">
+        <li><input type="text" class="form-control" placeholder="搜索"></li>
+        <li class="active " @click="popup" data-toggle="modal" data-target="#myModal">筛选<i class="fa fa-empire"></i></li>
+      </ol>
+    </div>
+    <!-- 筛选模态框 -->
+    <div :class="show" class="modal" id="myModal">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <!-- 模态框主体 -->
+          <div class="modal-body">
+            <div class="product-list-main">
+              <ul class="product-list-wrap" id="ul_select">
+                <li class="select-result">
+                  <dl id="dl_result" class="clearfix">
+                    <dd class="select-no"></dd>
+                    <dt v-on:click="clear"><span id="clearAll">清空筛选</span></dt>
+                    <dd v-for="item in selected" class="selected" v-on:click="del($event, Object.keys(item)[0])" v-bind:key="Object.keys(item)[0]"><span>{{ m[Object.keys(item)[0]] }}: {{ Object.values(item)[0] }}</span></dd>
+                  </dl>
+                </li>
+                <li v-if="kindsOn" class="select-list">
+                  <dl id="select1" class="dl clearfix">
+                    <dt>类别:</dt>
+                    <div class="fl">
+                      <dd v-for="item in kinds" v-on:click="kindsClick($event, item.name)" v-bind:key="item.name"><span>{{ item.name }}</span></dd>
+                    </div>
+                  </dl>
+                </li>
+                <li v-if="classesOn" class="select-list">
+                  <dl id="select2" class="dl clearfix">
+                    <dt>课程:</dt>
+                    <div class="fl">
+                      <dd v-for="item in classes" v-on:click="classesClick($event, item.name)" v-bind:key="item.name"><span>{{ item.name }}</span></dd>
+                    </div>
+                    <div class="product-list-more"><i>更多</i><img src="images/more.png" alt=""></div>
+                  </dl>
+                </li>
+                <li v-if="teachersOn" class="select-list">
+                  <dl id="select3" class="dl clearfix">
+                    <dt>教师:</dt>
+                    <div class="fl">
+                      <dd v-for="item in teachers" v-on:click="teachersClick($event, item.name)" v-bind:key="item.name"><span>{{ item.name }}</span></dd>
+                    </div>
+                    <div class="product-list-more"><i>更多</i><img src="images/more.png" alt=""></div>
+                  </dl>
+                </li>
+                <li v-if="timesOn" class="select-list">
+                  <dl id="select4" class="dl clearfix">
+                    <dt>时间:</dt>
+                    <div class="fl">
+                      <dd v-for="item in times" v-on:click="timesClick($event, item.length)" v-bind:key="item.length"><span>{{ item.length }}</span></dd>
+                    </div>
+                    <div class="product-list-more"><i>更多</i><img src="images/more.png" alt=""></div>
+                  </dl>
+                </li>
+                <li v-if="othersOn" class="select-list">
+                  <dl id="select5" class="dl clearfix">
+                    <dt>其他:</dt>
+                    <div class="fl">
+                      <dd v-for="item in others" v-on:click="othersClick($event, item.content)" v-bind:key="item.content"><span>{{ item.content }}</span></dd>
+                    </div>
+                    <div class="product-list-more"><i>更多</i><img src="images/more.png" alt=""></div>
+                  </dl>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <!-- 模态框底部 -->
+          <div class="modal-footer">
+            <button v-on:click="submit" type="button" class="btn btn-secondary" data-dismiss="modal">确认</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-    import api from '@/api'
     import store from '@/views/Home/store'
 
     export default {
         name: "Search",
+        computed: {
+            selected: function () {
+                return store.state.select
+            }
+        },
         data () {
             return {
-                keyword: '',
-                conditions: {
-                    kind: [
-                        {type: '类别1'},
-                        {type: '类别2'},
-                        {type: '类别3'}
-                    ],
-                    class: [
-                        {type: '课程1'},
-                        {type: '课程2'},
-                        {type: '课程3'}
-                    ],
-                    teacher: [
-                        {type: '何老师'},
-                        {type: '敏院长'},
-                        {type: '张老师'}
-                    ],
-                    time: [
-                        {type: '1'},
-                        {type: '2'},
-                        {type: '3'}
-                    ],
-                    other: [
-                        {type: '其它1'},
-                        {type: '其它2'},
-                        {type: '其它3'}
-                    ]
+                m: {
+                    kind: '类别',
+                    class: '课程',
+                    teacher: '老师',
+                    time: '时间',
+                    other: '其它'
                 },
-                selected: {
-                    kind: '',
-                    class: '',
-                    teacher: '',
-                    time: '',
-                    other: ''
-                },
+                show: "on-hidden",
+                kindsOn: true,
+                kinds: [
+                    {name: '类别1'},
+                    {name: '22'},
+                    {name: '33'}
+                ],
+                classesOn: true,
+                classes: [
+                    {name: '课程1'},
+                    {name: '22'},
+                    {name: '33'},
+                    {name: '44'},
+                    {name: '55'}
+                ],
+                teachersOn: true,
+                teachers: [
+                    {name: '何老师'},
+                    {name: '张老师'},
+                    {name: '敏院长'}
+                ],
+                timesOn: true,
+                times: [
+                    {length: '1'},
+                    {length: '2'},
+                    {length: '3'},
+                    {length: '4'}
+                ],
+                othersOn: true,
+                others: [
+                    {content: 'one'},
+                    {content: 'two'},
+                    {content: 'three'}
+                ]
             }
         },
         methods: {
-            search () {
-                api.searchByKeyword({keyword: this.keyword}).then(res => {
-                    store.commit('addCourses', res.data)
-                });
+            popup: function () {
+                this.show = "on-show"
             },
-            filter () {
-                api.searchByCondition(this.selected).then(res => {
-                    store.commit('addCourses', res.data)
-                });
+            kindsClick: function ($event, name) {
+                this.kindsOn = false;
+                store.commit('select', {kind: name})
+            },
+            classesClick: function ($event, name) {
+                this.classesOn = false;
+                store.commit('select', {class: name})
+            },
+            teachersClick: function ($event, name) {
+                this.teachersOn = false;
+                store.commit('select', {teacher: name})
+            },
+            timesClick: function ($event, length) {
+                this.timesOn = false;
+                store.commit('select', {time: length})
+            },
+            othersClick: function ($event, content) {
+                this.othersOn = false;
+                store.commit('select', {other: content})
+            },
+            del: function ($event, key) {
+                switch (key) {
+                    case 'kind':
+                        this.kindsOn = true;
+                        break;
+                    case 'class':
+                        this.classesOn = true;
+                        break;
+                    case 'teacher':
+                        this.teachersOn = true;
+                        break;
+                    case 'time':
+                        this.timesOn = true;
+                        break;
+                    case 'other':
+                        this.othersOn = true;
+                        break;
+                }
+                store.commit('delete', key);
+            },
+            clear: function () {
+                this.kindsOn = this.classesOn = this.teachersOn = this.timesOn = this.othersOn = true;
+                store.commit('clear');
+            },
+            submit: function () {
+                store.commit('filter');
+                store.commit('clear');
             }
         }
     }
 </script>
 
-<style scoped lang="scss">
-  .search {
-    width: 15%;
+<style scoped>
+  @import "../../assets/plugin/font-awesome/css/font-awesome.css";
+  @import '../../assets/css/iconfont.css';
+
+  .search-box-wrapper {
+    overflow: hidden;
   }
 
-  .filter {
-    width: 13%;
+  .on-show {
+    display: block;
+  }
+
+  .on-hidden {
+    display: none;
   }
 </style>
