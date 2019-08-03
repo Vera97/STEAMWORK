b4fd250bf46964365c9baf04cf77ff162d33eaba
MHX789 <1356794938@qq.com>
2019-08-04 01:57:14 +0800

    update new details of resource-show and class-list

--- src/components/startClass/new-comp.vue
+++ src/components/startClass/new-comp.vue
@@ -1,47 +1,48 @@
 <template>
-    <div>
-        <el-card class="ppt-box">
-            <i class="el-icon-close" @click="open(current)"></i>
-            <component v-bind:is="currentTabComponent" :exerciseId="exercise.exerciseId" ></component>
-        </el-card>
-    </div>
+  <div>
+    <el-card class="ppt-box">
+      <i class="el-icon-close" @click="open(current)"></i>
+      <component v-bind:is="currentTabComponent" :exerciseId="exercise.exerciseId"></component>
+    </el-card>
+  </div>
 </template>
 <script>
     import resourceShow from "../../components/startClass/resource-show";
+
     export default {
-      name:'new-comp',
-      components: {resourceShow},
-      props:['exercise'],
-      data() {
-        return {
-          current: true,
-          currentTabComponent:''
-      }
-    },
-    methods: {
-      open(current){
-        this.$emit('onEmmitCurrent', current)
-      },
-      getCurrentComponent(){
-          if(this.exercise.type ==='资源播放')
-          {
-            this.currentTabComponent='resource-show';
-            console.log(this.currentTabComponent);
-          }
-        }
-      },
-        created(){
-          this.getCurrentComponent();
+        name: 'new-comp',
+        components: {resourceShow},
+        props: ['exercise'],
+        data() {
+            return {
+                current: true,
+                currentTabComponent: ''
+            }
+        },
+        methods: {
+            open(current) {
+                this.$emit('onEmmitCurrent', current)
+            },
+            getCurrentComponent() {
+                if (this.exercise.type === '资源播放') {
+                    this.currentTabComponent = 'resource-show';
+                    console.log(this.currentTabComponent);
+                }
+            }
+        },
+        created() {
+            this.getCurrentComponent();
         }
     }
 </script>
 
 <style scoped>
-    .ppt-box{
-        text-align: center;
-        height:463px;
-    }
-    .el-icon-close{
-        float:right;
-    }
+  .ppt-box {
+    text-align: center;
+    height: 463px;
+  }
+
+  .el-icon-close {
+    float: right;
+  }
 </style>
--- src/components/startClass/resource-show.vue
+++ src/components/startClass/resource-show.vue
@@ -1,43 +1,54 @@
 <template>
-    <div>
-    <video class="video" controls>
-    <source src="video.mediaUrl" type="video.type">
+  <div>
+    <video class="videoPlay" controls>
+      <source src="video" type="videoType">
     </video>
-    </div>
+  </div>
 </template>
 
 <script>
-  import store from '../../store'
-  import {api, fakeData} from '../../api'
-  import utils from '../../utils'
-  export default {
-    name: "resource-show",
-    props:['exerciseId'],
-    computed:{
-       video(){
-         return store.state.startClass.exerciseMedia
-       }
-    },
-    created(){
-      utils.request({
-        invoke: api.requestExerciseMedia,
-        params: {
-          exerciseId:this.exerciseId
+    import store from '../../store'
+    import {api, fakeData} from '../../api'
+    import utils from '../../utils'
+
+    export default {
+        name: "resource-show",
+        props: ['exerciseId'],
+        computed: {
+            video() {
+                return store.state.startClass.exerciseMedia.mediaUrl
+            }
         },
-        result: fakeData.EXERCISE_RESOURCE
-      })
-          .then(res => {
-            store.commit('startClass/PLAY_RESOURCE', res.data);
-            console.log(res.data);
-          })
+        data() {
+            return {
+                videoType: ''
+            }
+        },
+        methods: {
+            getType(index) {
+                this.videoType = "video/" + index
+            }
+        },
+        created() {
+            utils.request({
+                invoke: api.requestExerciseMedia,
+                params: {
+                    exerciseId: this.exerciseId
+                },
+                result: fakeData.EXERCISE_RESOURCE
+            })
+                .then(res => {
+                    store.commit('startClass/PLAY_RESOURCE', res.data);
+                    this.getType(res.data.fileType);
+                });
+        }
     }
-  }
 </script>
 
 <style scoped>
-    .video{
-        margin-top:10px;
-        width:100%;
-        height:400px;
-    }
+  .videoPlay {
+    margin-top: 10px;
+    width: 100%;
+    height: 400px;
+  }
 </style>
