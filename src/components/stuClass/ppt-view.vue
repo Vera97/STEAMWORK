712e2f1171283108877adf7944def7d7db4c1404
BigSaltyFish <2983483048@qq.com>
2019-08-28 22:27:19 +0800

    now the admin can login, the students won't see activities which should be visible only to teachers, i.e. partition, checking attendance. students will see whether they have completed the activities, a click on the completed activity will only cause a prompt to inform the student. the students will be rewarded will wealth when complete activities which haven't expired. other feature is still being negotiated.

--- src/components/stuClass/ppt-view.vue
+++ src/components/stuClass/ppt-view.vue
@@ -125,12 +125,15 @@
                     }.bind(this))
             },
             openAct(item) {//点击活动按钮
+                if (item.state === 1) {
+                    console.log(item);
+                    this.$message.success('此活动已完成');
+                    return
+                }
                 this.requestActivityStatus(item)
                     .then(function(data) {
-                        console.log(data);
                         if (data.code === 1) {
                             this.cur = false;
-                            console.log(this.$refs);
                             this.$refs.actList.load({
                                 exerciseType: item.type,
                                 ...data
@@ -171,14 +174,31 @@
                     result: fakeData.IS_START
                 })
                     .then(function (res) {
-                        return Promise.resolve(res.data)
+                        console.log(`not null: ${item.title}`);
+                        return Promise.resolve({
+                            ...res.data,
+                            exerciseId: item.exerciseId,
+                            type: item.type,
+                            title: item.title
+                        })
                     })
             },
             onEmmitCur() {//接受从act-list关闭按钮传值，以关闭活动组件
                 this.cur = true;
             },
-            getAct() {//向后端请求活动列表
-                utils.request({
+            async getAct() {//向后端请求活动列表
+                let status = await utils.request({
+                    invoke: api.requestExerciseState,
+                    params: {
+                        stuId: store.state.stuId,
+                        courseSectionId: store.state.courseSectionId
+                    },
+                    result: fakeData.GET_EXERCISE_STATE_COMPLETED
+                })
+                    .then(function (res) {
+                        return Promise.resolve(res.data.exerciseStateList)
+                    }.bind(this));
+                let list = await utils.request({
                     invoke: api.requestExercise,
                     params: {
                         pptId: this.pptId,
@@ -186,9 +206,20 @@
                     },
                     result: fakeData.STU_EXERCISE_LIST
                 })
-                    .then(res => {
-                        store.commit('stuClass/ADD_EXERCISE', res.data.exerciseList);
-                    })
+                    .then(function (res) {
+                        return Promise.resolve(res.data.exerciseList)
+                    });
+                list = list.filter(item => {
+                    return item.type === '文本播放' || item.type === '互动问答' || item.type === '资源播放';
+                });
+                for (let i = 0; i < list.length; i++) {
+                    for (let j of status) {
+                        if (j.exerciseId === list[i].exerciseId) {
+                            list[i].state = j.state ? 1 : 0
+                        }
+                    }
+                }
+                store.commit('stuClass/ADD_EXERCISE', list);
             },
             addWealth() {//修改财富值
                 utils.request({
